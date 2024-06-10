66af3376825cc8a17cda7cd05c90b519c6ef70a0
ousttrue <ousttrue@gmail.com>
2024-06-11 03:45:56 +0900

    raylib Camera3D

--- examples/tiny-gizmo-example/example-main.cpp
+++ examples/tiny-gizmo-example/example-main.cpp
@@ -4,11 +4,12 @@
 
 #include "gizmo.h"
 
-#include "camera.h"
+// #include "camera.h"
 #include "drawable.h"
 #include "teapot.h"
 
 #include <raymath.h>
+#include <rcamera.h>
 #include <rlgl.h>
 
 #include <chrono>
@@ -19,12 +20,82 @@ struct TeapotVertex {
   Vector3 color;
 };
 
+auto dolly(Camera3D *camera) -> void {
+  auto wheel = GetMouseWheelMoveV();
+  if (wheel.y > 0) {
+    auto distance = Vector3Distance(camera->target, camera->position);
+    CameraMoveToTarget(camera, distance * 0.9 - distance);
+  } else if (wheel.y < 0) {
+    auto distance = Vector3Distance(camera->target, camera->position);
+    CameraMoveToTarget(camera, distance * 1.1 - distance);
+  }
+}
+
+struct OrbitCamera {
+  int yawDegree = 0;
+  int pitchDegree = 40;
+  float shiftX = 0;
+  float shiftY = 0;
+
+  bool MouseUpdateCamera(float distance, float fovy, const Rectangle &rect) {
+    auto delta = GetMouseDelta();
+    auto active = false;
+
+    // camera shift
+    if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)) {
+      auto speed = distance * tan(fovy * 0.5) * 2.0 / rect.height;
+      shiftX += delta.x * speed;
+      shiftY += delta.y * speed;
+      active = true;
+    }
+
+    // yaw pitch
+    if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+      yawDegree -= static_cast<int>(delta.x);
+      pitchDegree += static_cast<int>(delta.y);
+      if (pitchDegree > 89) {
+        pitchDegree = 89;
+      } else if (pitchDegree < -89) {
+        pitchDegree = -89;
+      }
+      active = true;
+    }
+
+    return active;
+  }
+
+  void update_view(Camera3D *camera) {
+    auto distance = Vector3Distance(camera->target, camera->position);
+    auto pitch = MatrixRotateX(static_cast<float>(pitchDegree) * DEG2RAD);
+    auto yaw = MatrixRotateY(static_cast<float>(yawDegree) * DEG2RAD);
+    auto translation = MatrixTranslate(shiftX, shiftY, -distance);
+    auto camera_transform =
+        MatrixMultiply(translation, MatrixMultiply(pitch, yaw));
+
+    camera->position = {
+        .x = camera_transform.m12,
+        .y = camera_transform.m13,
+        .z = camera_transform.m14,
+    };
+    auto forward = Vector3{
+        .x = camera_transform.m8,
+        .y = camera_transform.m9,
+        .z = camera_transform.m10,
+    };
+    camera->target =
+        Vector3Add(camera->position, Vector3Scale(forward, distance));
+  }
+};
+
 int main(int argc, char *argv[]) {
-  camera cam = {};
-  cam.yfov = 1.0f;
-  cam.near_clip = 0.01f;
-  cam.far_clip = 32.0f;
-  cam.position = {0, 1.5f, 4};
+
+  Camera3D camera{
+      .position = {0, 1.5f, 10},
+      .target = {0, 0, 0},
+      .up = {0, 1, 0},
+      .fovy = 45.0f,
+      .projection = CAMERA_PERSPECTIVE,
+  };
 
   InitWindow(1280, 800, "tiny-gizmo-example-app");
 
@@ -41,26 +112,39 @@ int main(int argc, char *argv[]) {
   xform_b.position = {2, 0, 0};
 
   Gizmo gizmo;
+  OrbitCamera orbit;
 
   auto t0 = std::chrono::high_resolution_clock::now();
   while (!WindowShouldClose()) {
-    auto w = GetScreenWidth();
-    auto h = GetScreenHeight();
-    auto aspect = static_cast<float>(w) / static_cast<float>(h);
-    auto t1 = std::chrono::high_resolution_clock::now();
-    float timestep = std::chrono::duration<float>(t1 - t0).count();
-    t0 = t1;
+    // auto w = GetScreenWidth();
+    // auto h = GetScreenHeight();
+    // auto aspect = static_cast<float>(w) / static_cast<float>(h);
+    // auto t1 = std::chrono::high_resolution_clock::now();
+    // float timestep = std::chrono::duration<float>(t1 - t0).count();
+    // t0 = t1;
 
     // mouse
-    auto mr = IsMouseButtonDown(MOUSE_BUTTON_RIGHT);
-    auto deltaCursorMotion = GetMouseDelta();
-    if (mr) {
-      cam.yaw -= deltaCursorMotion.x * 0.01f;
-      cam.pitch -= deltaCursorMotion.y * 0.01f;
-    }
-    if (mr) {
-      cam.shift(timestep);
-    }
+    // auto mr = IsMouseButtonDown(MOUSE_BUTTON_RIGHT);
+    // auto deltaCursorMotion = GetMouseDelta();
+    // if (mr) {
+    //   yaw -= deltaCursorMotion.x * 0.01f;
+    //   pitch -= deltaCursorMotion.y * 0.01f;
+    // }
+    // if (mr) {
+    //   cam.shift(timestep);
+    // }
+    dolly(&camera);
+    auto distance = Vector3Distance(camera.target, camera.position);
+    orbit.MouseUpdateCamera(distance, camera.fovy,
+                            {
+                                0,
+                                0,
+                                static_cast<float>(GetScreenWidth()),
+                                static_cast<float>(GetScreenHeight()),
+                            });
+    // if (self.is_active) {
+    orbit.update_view(&camera);
+    // }
 
     // gizmo
     gizmo.new_frame();
@@ -77,17 +161,8 @@ int main(int argc, char *argv[]) {
     glDisable(GL_CULL_FACE);
     ClearBackground(RAYWHITE);
 
+    BeginMode3D(camera);
     {
-      auto proj = cam.get_projection_matrix(aspect);
-      rlMatrixMode(RL_PROJECTION); // Switch to projection matrix
-      rlLoadIdentity();            // Reset current matrix (projection)
-      rlMultMatrixf(&proj.x.x);
-
-      rlMatrixMode(RL_MODELVIEW); // Switch to projection matrix
-      rlLoadIdentity();           // Reset current matrix (projection)
-      auto view = cam.get_view_matrix();
-      rlMultMatrixf(&view.x.x);
-
       // teapot
       teapot.draw(MatrixTranspose(*(Matrix *)&ma));
       teapot.draw(MatrixTranspose(*(Matrix *)&mb));
@@ -95,9 +170,9 @@ int main(int argc, char *argv[]) {
       DrawGrid(10, 1.0);
 
       gizmo.draw();
-
-      EndDrawing();
     }
+    EndMode3D();
+    EndDrawing();
   }
 
   CloseWindow();
