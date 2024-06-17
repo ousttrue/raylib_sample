a5c9c8498348fc9388afb05f3f6277f5d00b2469
ousttrue <ousttrue@gmail.com>
2024-06-17 21:46:29 +0900

    translation_xyz_component

--- tinygizmo/tinygizmo_translation.cpp
+++ tinygizmo/tinygizmo_translation.cpp
@@ -6,34 +6,69 @@ namespace tinygizmo {
 std::vector<minalg::float2> arrow_points = {
     {0.25f, 0}, {0.25f, 0.05f}, {1, 0.05f}, {1, 0.10f}, {1.2f, 0}};
 
-auto _translate_x = std::make_shared<gizmo_component>(
-    geometry_mesh::make_lathed_geometry({1, 0, 0}, {0, 1, 0}, {0, 0, 1}, 16,
-                                        arrow_points),
-    minalg::float4{1, 0.5f, 0.5f, 1.f}, minalg::float4{1, 0, 0, 1.f});
-auto _translate_y = std::make_shared<gizmo_component>(
-    geometry_mesh::make_lathed_geometry({0, 1, 0}, {0, 0, 1}, {1, 0, 0}, 16,
-                                        arrow_points),
-    minalg::float4{0.5f, 1, 0.5f, 1.f}, minalg::float4{0, 1, 0, 1.f});
-auto _translate_z = std::make_shared<gizmo_component>(
-    geometry_mesh::make_lathed_geometry({0, 0, 1}, {1, 0, 0}, {0, 1, 0}, 16,
-                                        arrow_points),
-    minalg::float4{0.5f, 0.5f, 1, 1.f}, minalg::float4{0, 0, 1, 1.f});
-auto _translate_yz = std::make_shared<gizmo_component>(
-    geometry_mesh::make_box_geometry({-0.01f, 0.25, 0.25},
-                                     {0.01f, 0.75f, 0.75f}),
-    minalg::float4{0.5f, 1, 1, 0.5f}, minalg::float4{0, 1, 1, 0.6f});
-auto _translate_zx = std::make_shared<gizmo_component>(
-    geometry_mesh::make_box_geometry({0.25, -0.01f, 0.25},
-                                     {0.75f, 0.01f, 0.75f}),
-    minalg::float4{1, 0.5f, 1, 0.5f}, minalg::float4{1, 0, 1, 0.6f});
-auto _translate_xy = std::make_shared<gizmo_component>(
-    geometry_mesh::make_box_geometry({0.25, 0.25, -0.01f},
-                                     {0.75f, 0.75f, 0.01f}),
-    minalg::float4{1, 1, 0.5f, 0.5f}, minalg::float4{1, 1, 0, 0.6f});
-auto _translate_xyz = std::make_shared<gizmo_component>(
-    geometry_mesh::make_box_geometry({-0.05f, -0.05f, -0.05f},
-                                     {0.05f, 0.05f, 0.05f}),
-    minalg::float4{0.9f, 0.9f, 0.9f, 0.25f}, minalg::float4{1, 1, 1, 0.35f});
+struct translation_x_component : gizmo_component {
+  translation_x_component()
+      : gizmo_component(geometry_mesh::make_lathed_geometry(
+                            {1, 0, 0}, {0, 1, 0}, {0, 0, 1}, 16, arrow_points),
+                        minalg::float4{1, 0.5f, 0.5f, 1.f},
+                        minalg::float4{1, 0, 0, 1.f}) {}
+};
+
+struct translation_y_component : gizmo_component {
+  translation_y_component()
+      : gizmo_component(geometry_mesh::make_lathed_geometry(
+                            {0, 1, 0}, {0, 0, 1}, {1, 0, 0}, 16, arrow_points),
+                        minalg::float4{0.5f, 1, 0.5f, 1.f},
+                        minalg::float4{0, 1, 0, 1.f}) {}
+};
+
+struct translation_z_component : gizmo_component {
+  translation_z_component()
+      : gizmo_component(geometry_mesh::make_lathed_geometry(
+                            {0, 0, 1}, {1, 0, 0}, {0, 1, 0}, 16, arrow_points),
+                        minalg::float4{0.5f, 0.5f, 1, 1.f},
+                        minalg::float4{0, 0, 1, 1.f}) {}
+};
+
+struct translation_yz_component : gizmo_component {
+  translation_yz_component()
+      : gizmo_component(geometry_mesh::make_box_geometry({-0.01f, 0.25, 0.25},
+                                                         {0.01f, 0.75f, 0.75f}),
+                        minalg::float4{0.5f, 1, 1, 0.5f},
+                        minalg::float4{0, 1, 1, 0.6f}) {}
+};
+
+struct translation_zx_component : gizmo_component {
+  translation_zx_component()
+      : gizmo_component(geometry_mesh::make_box_geometry({0.25, -0.01f, 0.25},
+                                                         {0.75f, 0.01f, 0.75f}),
+                        minalg::float4{1, 0.5f, 1, 0.5f},
+                        minalg::float4{1, 0, 1, 0.6f}) {}
+};
+
+struct translation_xy_component : gizmo_component {
+  translation_xy_component()
+      : gizmo_component(geometry_mesh::make_box_geometry({0.25, 0.25, -0.01f},
+                                                         {0.75f, 0.75f, 0.01f}),
+                        minalg::float4{1, 1, 0.5f, 0.5f},
+                        minalg::float4{1, 1, 0, 0.6f}) {}
+};
+
+struct translation_xyz_component : gizmo_component {
+  translation_xyz_component()
+      : gizmo_component(geometry_mesh::make_box_geometry(
+                            {-0.05f, -0.05f, -0.05f}, {0.05f, 0.05f, 0.05f}),
+                        minalg::float4{0.9f, 0.9f, 0.9f, 0.25f},
+                        minalg::float4{1, 1, 1, 0.35f}) {}
+};
+
+auto _translate_x = std::make_shared<translation_x_component>();
+auto _translate_y = std::make_shared<translation_y_component>();
+auto _translate_z = std::make_shared<translation_z_component>();
+auto _translate_yz = std::make_shared<translation_yz_component>();
+auto _translate_zx = std::make_shared<translation_zx_component>();
+auto _translate_xy = std::make_shared<translation_xy_component>();
+auto _translate_xyz = std::make_shared<translation_xyz_component>();
 
 std::tuple<std::shared_ptr<gizmo_component>, float>
 position_intersect(const ray &ray) {
