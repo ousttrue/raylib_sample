f5587a598e0215ce4ce32b058bd3ce3e491fef9e
ousttrue <ousttrue@gmail.com>
2024-06-21 10:09:16 +0900

    begin_camera3D

--- src/main.zig
+++ src/main.zig
@@ -38,28 +38,6 @@ fn draw_frustum(frustum: zamath.Frustum) void {
     c.DrawLine3D(tor(frustum.far_right_top), tor(frustum.far_left_top), c.DARKBLUE);
 }
 
-pub fn begin_camera3D(projection: *const f32, view: *const f32) void {
-    c.rlDrawRenderBatchActive(); // Update and draw internal render batch
-
-    {
-        c.rlMatrixMode(c.RL_PROJECTION);
-        c.rlPushMatrix();
-        c.rlLoadIdentity();
-        c.rlMultMatrixf(projection);
-    }
-    {
-        c.rlMatrixMode(c.RL_MODELVIEW);
-        c.rlLoadIdentity();
-        c.rlMultMatrixf(view);
-    }
-
-    c.rlEnableDepthTest(); // Enable DEPTH_TEST for 3D
-}
-
-pub fn end_camera3D() void {
-    c.EndMode3D();
-}
-
 const Scene = struct {
     cubePosition: layout.c.Vector3 = .{ .x = 0.0, .y = 0.0, .z = 0.0 },
 
@@ -120,13 +98,26 @@ const View = struct {
         }
     }
 
-    pub fn draw(self: @This(), scene: *Scene) void {
-        begin_camera3D(
-            &self.camera.projection_matrix.m00,
-            &self.camera.view_matrix.m00,
-        );
-        scene.draw();
-        end_camera3D();
+    pub fn begin_camera3D(self: *const @This()) void {
+        c.rlDrawRenderBatchActive(); // Update and draw internal render batch
+
+        {
+            c.rlMatrixMode(c.RL_PROJECTION);
+            c.rlPushMatrix();
+            c.rlLoadIdentity();
+            c.rlMultMatrixf(&self.camera.projection_matrix.m00);
+        }
+        {
+            c.rlMatrixMode(c.RL_MODELVIEW);
+            c.rlLoadIdentity();
+            c.rlMultMatrixf(&self.camera.view_matrix.m00);
+        }
+
+        c.rlEnableDepthTest(); // Enable DEPTH_TEST for 3D
+    }
+
+    pub fn end_camera3D(_: @This()) void {
+        c.EndMode3D();
     }
 
     pub fn mouse_near_far(self: @This()) struct { zamath.Vec3, zamath.Vec3 } {
@@ -161,8 +152,7 @@ pub fn main() !void {
     c.SetTargetFPS(60);
     c.rlImGuiSetup(true);
     defer c.rlImGuiShutdown();
-
-    var scene = Scene{};
+    const io = c.igGetIO();
 
     // Custom timming variables
     var previousTime: f64 = c.GetTime(); // Previous time measure
@@ -170,14 +160,12 @@ pub fn main() !void {
     var updateDrawTime: f64 = 0.0; // Update + Draw time
     var deltaTime: f64 = 0.0; // Frame time (Update + Draw + Wait time)
 
-    var root_view = View{};
+    var scene: Scene = .{};
+    var root_view: View = .{};
+    var rendertarget: layout.RenderTarget = .{};
+    var fbo_view: View = .{};
     root_view.update_view_matrix();
-    var fbo_view = View{};
     fbo_view.update_view_matrix();
-
-    var rendertarget = layout.RenderTarget{};
-
-    const io = c.igGetIO();
 
     while (!c.WindowShouldClose()) {
         const w: f32 = @floatFromInt(c.GetScreenWidth());
@@ -209,92 +197,134 @@ pub fn main() !void {
             }
         }
 
+        // show ImGui Content
+        {
+            c.rlImGuiBegin();
+
+            var open = true;
+            c.igShowDemoWindow(&open);
+
+            {
+                c.igPushStyleVar_Vec2(c.ImGuiStyleVar_WindowPadding, .{ .x = 0, .y = 0 });
+                defer c.igPopStyleVar(1);
+                var open_fbo = true;
+                if (c.igBegin("fbo", &open_fbo, c.ImGuiWindowFlags_NoScrollbar | c.ImGuiWindowFlags_NoScrollWithMouse)) {
+                    c.igPushStyleVar_Vec2(c.ImGuiStyleVar_FramePadding, .{ .x = 0, .y = 0 });
+                    defer c.igPopStyleVar(1);
+                    var pos = c.ImVec2{};
+                    c.igGetCursorScreenPos(@ptrCast(&pos));
+                    var size = c.ImVec2{};
+                    c.igGetContentRegionAvail(@ptrCast(&size));
+
+                    if (size.x > 0 and size.y > 0) {
+                        if (fbo_view.camera.set_viewport_cursor(
+                            pos.x,
+                            pos.y,
+                            size.x,
+                            size.y,
+                            cursor.x,
+                            cursor.y,
+                        )) {
+                            fbo_view.update_projection_matrix();
+                        }
+
+                        if (rendertarget.begin(@intFromFloat(size.x), @intFromFloat(size.y))) |texture| {
+                            _ = c.igImageButton(
+                                "fbo",
+                                @constCast(@ptrCast(&texture)),
+                                size,
+                                .{ .x = 0, .y = 1 },
+                                .{ .x = 1, .y = 0 },
+                                .{ .x = 1, .y = 1, .z = 1, .w = 1 },
+                                .{ .x = 1, .y = 1, .z = 1, .w = 1 },
+                            );
+                            Custom_ButtonBehaviorMiddleRight();
+
+                            if (c.igIsItemActive()) {
+                                fbo_view.update(
+                                    cursor_delta.x,
+                                    cursor_delta.y,
+                                    size.y,
+                                );
+                            } else if (c.igIsItemHovered(0)) {
+                                if (wheel.y != 0) {
+                                    fbo_view.camera_orbit.dolly(wheel.y);
+                                    fbo_view.update_view_matrix();
+                                }
+                            }
+
+                            {
+                                fbo_view.begin_camera3D();
+                                defer fbo_view.end_camera3D();
+
+                                draw_frustum(root_view.frustum());
+                                const start, const end = root_view.mouse_near_far();
+                                c.DrawLine3D(tor(start), tor(end), c.YELLOW);
+                                scene.draw();
+                            }
+
+                            rendertarget.end();
+                        }
+                    }
+                }
+                c.igEnd();
+            }
+
+            {
+                _ = c.igBegin("root view", null, 0);
+                defer c.igEnd();
+
+                var start, var end = root_view.mouse_near_far();
+                _ = c.igInputFloat3("start", &start.x, "%.3f", 0);
+                _ = c.igInputFloat3("end", &end.x, "%.3f", 0);
+
+                //   ImGui::ColorEdit3("clear color", ClearColor);
+
+                // camera
+                //   ImGui::Separator();
+                //   ImGui::TextUnformatted("[camera]");
+                //   ImGui::DragFloat("camera near", &Camera.Projection.NearZ);
+                //   Camera.Projection.NearZ = std::max(
+                //       0.01f, std::min(Camera.Projection.NearZ, Camera.Projection.FarZ - 1));
+                //   ImGui::DragFloat("camera far", &Camera.Projection.FarZ);
+                //   ImGui::InputFloat3("camera pos", &Camera.Transform.Translation.x);
+                //   Camera.Projection.FarZ =
+                //       std::max(Camera.Projection.FarZ, Camera.Projection.NearZ + 1);
+                //
+                //   if (auto ray = Gui.m_context.Ray) {
+                //     ImGui::BeginDisabled(false);
+                //     ImGui::InputFloat3("ray dir", &ray->Direction.x);
+                //   } else {
+                //     float zero[3]{0, 0, 0};
+                //     ImGui::InputFloat3("ray dir", zero);
+                //     ImGui::BeginDisabled(true);
+                //   }
+                //
+                //   ImGui::TextUnformatted("ray hits");
+                //   for (auto hit : Gui.m_hits) {
+                //     ImGui::Text("hit: %0.3f", hit);
+                //   }
+                //
+                //   ImGui::EndDisabled();
+                // }
+            }
+        }
+
         {
             c.BeginDrawing();
             c.ClearBackground(c.RAYWHITE);
-            root_view.draw(&scene);
+
             {
-                draw_frustum(fbo_view.frustum());
-                // mouse ray
-                const start, const end = fbo_view.mouse_near_far();
-                c.DrawLine3D(tor(start), tor(end), c.YELLOW);
+                root_view.begin_camera3D();
+                defer root_view.end_camera3D();
+
+                scene.draw();
             }
 
             c.DrawText(c.TextFormat("CURRENT FPS: %.0f", (1.0 / deltaTime)), c.GetScreenWidth() - 220, 40, 20, c.GREEN);
 
-            {
-                // IMGUI
-                c.rlImGuiBegin();
-
-                {
-                    c.igPushStyleVar_Vec2(c.ImGuiStyleVar_WindowPadding, .{ .x = 0, .y = 0 });
-                    defer c.igPopStyleVar(1);
-                    var open_fbo = true;
-                    if (c.igBegin("fbo", &open_fbo, c.ImGuiWindowFlags_NoScrollbar | c.ImGuiWindowFlags_NoScrollWithMouse)) {
-                        c.igPushStyleVar_Vec2(c.ImGuiStyleVar_FramePadding, .{ .x = 0, .y = 0 });
-                        defer c.igPopStyleVar(1);
-                        var pos = c.ImVec2{};
-                        c.igGetCursorScreenPos(@ptrCast(&pos));
-                        var size = c.ImVec2{};
-                        c.igGetContentRegionAvail(@ptrCast(&size));
-
-                        if (size.x > 0 and size.y > 0) {
-                            if (fbo_view.camera.set_viewport_cursor(
-                                pos.x,
-                                pos.y,
-                                size.x,
-                                size.y,
-                                cursor.x,
-                                cursor.y,
-                            )) {
-                                fbo_view.update_projection_matrix();
-                            }
-
-                            if (rendertarget.begin(@intFromFloat(size.x), @intFromFloat(size.y))) |texture| {
-                                _ = c.igImageButton(
-                                    "fbo",
-                                    @constCast(@ptrCast(&texture)),
-                                    size,
-                                    .{ .x = 0, .y = 1 },
-                                    .{ .x = 1, .y = 0 },
-                                    .{ .x = 1, .y = 1, .z = 1, .w = 1 },
-                                    .{ .x = 1, .y = 1, .z = 1, .w = 1 },
-                                );
-                                Custom_ButtonBehaviorMiddleRight();
-
-                                if (c.igIsItemActive()) {
-                                    fbo_view.update(
-                                        cursor_delta.x,
-                                        cursor_delta.y,
-                                        size.y,
-                                    );
-                                } else if (c.igIsItemHovered(0)) {
-                                    if (wheel.y != 0) {
-                                        fbo_view.camera_orbit.dolly(wheel.y);
-                                        fbo_view.update_view_matrix();
-                                    }
-                                }
-
-                                fbo_view.draw(&scene);
-                                draw_frustum(root_view.frustum());
-                                // mouse ray
-                                const start, const end = root_view.mouse_near_far();
-                                c.DrawLine3D(tor(start), tor(end), c.YELLOW);
-
-                                rendertarget.end();
-                            }
-                        }
-                    }
-                    c.igEnd();
-                }
-
-                // show ImGui Content
-                var open = true;
-                c.igShowDemoWindow(&open);
-
-                // end ImGui Content
-                c.rlImGuiEnd();
-            }
+            // end ImGui Content
+            c.rlImGuiEnd();
 
             c.EndDrawing();
         }
